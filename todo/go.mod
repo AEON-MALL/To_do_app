5316fe6712bfb02298ffe7f2d5c0a038868272f1
AEON-MALL <jbytq@yahoo.co.jp>
2022-01-10 23:54:36 +0900

    second commit

--- todo/go.mod
+++ todo/go.mod
@@ -2,4 +2,8 @@ module todo
 
 go 1.16
 
-require gopkg.in/go-ini/ini.v1 v1.66.2
+require (
+	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.10
+	gopkg.in/go-ini/ini.v1 v1.66.2
+)
--- todo/main.go
+++ todo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"todo/app/models"
 	"todo/config"
 )
 
@@ -10,4 +11,13 @@ func main() {
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.DbName)
 	fmt.Println(config.Config.LogFile)
+
+	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+	u.CreateUser()
 }
