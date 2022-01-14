39dc23aacc682514026d3bf6837559f008fbdba1
AEON-MALL <jbytq@yahoo.co.jp>
2022-01-14 22:34:18 +0900

    therd commit

--- todo/app/models/base.go
+++ todo/app/models/base.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
 	"log"
@@ -41,7 +42,7 @@ func createUUID() (uuidobj uuid.UUID){
 	return uuidobj 
 }
 
-func Encrypt(plaintext string) (cryptext string)
+func Encrypt(plaintext string) (cryptext string){
 	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
 	return cryptext
 }
