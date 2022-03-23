2cb903f97d584f6f9bd86b80f4f3d3e1a8a0200b
AEON-MALL <jbytq@yahoo.co.jp>
2022-03-24 00:42:19 +0900

    fix code

--- todo/todo/main.go
+++ todo/todo/main.go
@@ -2,23 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	//"todo/app/controllers"
+	"todo/app/controllers"
 	"todo/app/models"
-	//"todo/config"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	//controllers.StartMainServer()
+	controllers.StartMainServer()
 
-	user, _  := models.GetUserByEmail("test@example.com")
-	fmt.Println(user)
-
-	session, err := user.CreateSession()
-	if err != nil{
-		log.Println(err)
-	}
-	fmt.Println(session)
 }
